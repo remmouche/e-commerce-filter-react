3b51bb13f6f35351ca23b6749526f137d9244702
remmouche <121981104+remmouche@users.noreply.github.com>
2025-03-19 20:42:58 +0000

    add components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Navbar from "./pages/Navigation/Navbar";
+import Products from "./pages/Products/Products";
+import Recommended from "./pages/Recommended/Recommended";
+import Sidebar from "./pages/Sidebar/Sidebar";
+
 function App() {
   return (
     <>
       <h1>Bismi Allah</h1>
+      <Navbar />
+      <Products />
+      <Recommended />
+      <Sidebar />
     </>
   );
 }
