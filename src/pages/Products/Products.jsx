f1c3ad885813aab26fa915f5f8085062592da6af
remmouche <121981104+remmouche@users.noreply.github.com>
2025-03-21 02:19:06 +0000

    update card css

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,140 @@
-import React from 'react'
-import './Products.css'
+import React from "react";
+import "./Products.css";
+import { FaStar } from "react-icons/fa";
+import { IoBagAdd } from "react-icons/io5";
 
 function Products() {
   return (
-    <h2>Products</h2>
-  )
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            className="card-img"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="shoe"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-img"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="shoe"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>{" "}
+        <section className="card">
+          <img
+            className="card-img"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="shoe"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>{" "}
+        <section className="card">
+          <img
+            className="card-img"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="shoe"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>{" "}
+        <section className="card">
+          <img
+            className="card-img"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="shoe"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
 
-export default Products
+export default Products;
